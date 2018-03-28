aa348ca8a1ab97d197623f6cce66fc7131aded75
PedroG98 <37638970-PedroG98@users.noreply.github.com>
2018-03-28 22:47:46 +0200

    New fade test. Este es más eficiente
    PD: Parece que la capacidad de usar alphavalues depende del tipo de
    valor de pixel de la surface

---
+++ Juego(por nombrar)/test/better_fade_test_with_shitty_movement.py
@@ -0,0 +1,44 @@
+import pygame as pg
+from pygame.locals import *
+import math
+
+player_pos = [100,100]
+
+ball = pg.image.load("Juego(por nombrar)/src/clases/animations/ball_1.png")
+person = pg.image.load("Juego(por nombrar)/src/clases/animations/player_walk_d1.png")
+#hitbox_p = pg.Block((120,120,120), 20, 40)
+
+pg.init()
+screen = pg.display.set_mode((500,500))
+pg.display.set_caption("hitbox test")
+ball.convert()
+person.convert_alpha()
+person.set_alpha(255)
+
+t = 0
+while True:
+    pg.event.pump()
+    ky = pg.key.get_pressed()
+    if ky[K_ESCAPE]:
+        print(pg.display.Info())
+        pg.quit()
+    if ky[K_DOWN]:
+        player_pos[1] += 5
+    if ky[K_UP]:
+        player_pos[1] -= 5
+    if ky[K_LEFT]:
+        player_pos[0] -= 5
+    if ky[K_RIGHT]:
+        player_pos[0] += 5
+
+    screen.fill((255, 255, 255))
+    screen.blit(ball, (350,250), (0,0,100,100))
+    screen.blit(person, player_pos)
+
+    alpha = 200 +  55*math.sin(t)
+    person.set_alpha(alpha)
+    print(person.get_alpha())
+    #hitbox_p.x
+    t += 0.3
+    pg.time.delay(50)
+    pg.display.update()
