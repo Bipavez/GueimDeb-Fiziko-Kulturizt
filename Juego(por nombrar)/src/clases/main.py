f8637f0c8d54c8f5720a7ed72ef95e1d22a57990
PedroG98 <37638970-PedroG98@users.noreply.github.com>
2018-03-30 00:45:08 +0200

    Fog update

--- Juego(por nombrar)/src/clases/main.py
+++ Juego(por nombrar)/src/clases/main.py
@@ -7,46 +7,69 @@ from Control import *
 from Sprites import *
 from ImageProcessing import *
 
-win_l = 700
-win_h = 500
+#GAME DISPLAY PARAMETERS            #Exportar esto a otro archivo
+W = 700
+H = 500
+
+FOG = 1
+#/
 
 player_x = 0
 player_y = 0
 
+def draw_fog(screen, depth):
+    fog = pg.Surface((W,H), pg.SRCALPHA)
+    fog.fill((0,0,0,255))
+    for i in range(255, 1, -1):
+        pg.draw.circle(fog, (0,0,0,i), (W//2,H//2), round(i*depth))
+    return fog
+
 pg.init()
-screen = pg.display.set_mode((win_l,win_h))
+screen = pg.display.set_mode((W,H))
 background = pg.image.load("animations\\background.png")
 
 player = Character_Sprite("player", 5)
 p_l, p_h = player.rect.width, player.rect.height
-player.set_position((win_l//2 - p_l//2, win_h//2 - p_h//2)) #Cambiar, limpiar
+player.set_position((W//2 - p_l//2, H//2 - p_h//2)) #Cambiar, limpiar
 
 ball = Item_Sprite("ball", player)
 
 npc = Character_Sprite("player",0)
 npc.set_position((400,300))
 shadow = Shadow(player)
-player_entities = sprite.Group(shadow ,player, ball)
+player_entities = sprite.Group(shadow, player, ball)
 background_entities = sprite.Group(npc)
+collision_entities = sprite.Group(player, npc)
 
 player_x = player.x
 player_y = player.y
 
 clock = pg.time.Clock()
 
+fog = draw_fog(screen, FOG)
+
 while True:
+    #Screen filling START
     screen.fill((0,0,0))
-    screen.blit(background, (win_l//2 - player.x, win_h//2 - player.y, win_l, win_h))
+    screen.blit(background, (W//2 - player.x, H//2 - player.y, W, H))
     background_entities.draw(background)
     player_entities.draw(screen)
-
-    if pg.key.get_pressed()[pg.K_ESCAPE]:
+    if FOG != 0:
+        screen.blit(fog, (0,0))
+    #Screen filling END
+        
+    keys = pg.key.get_pressed()
+    if keys[K_ESCAPE]:
         pg.quit() # quit the screen
         break
+    if keys[K_f]:
+        FOG = not FOG
+        if FOG:
+            fog = draw_fog(screen)
     for event in pg.event.get():
         if event.type is pg.QUIT:
             pg.quit() # quit the screen
-            break
+            break            
 
     player_entities.update()
     background_entities.update()
@@ -57,6 +80,5 @@ while True:
     handle_keys(player)
     player_x = player.x
     player_y = player.y
-    print("{}, {}".format(player_x, player_y))
     pg.display.update()
     background_entities.clear(background,background)
