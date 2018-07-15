cf63684c921eb499a6608f6c6d447b84b66f41a9
Benjamin Pavez <bipavez@uc.cl>
2018-07-15 12:24:26 -0400

    sdv

---
+++ Unity Proyect/Rebirth-2/Assets/Scripts/Character.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour {
+	[SerializeField]
+	private float speed;
+
+	protected Vector2 direction;
+	// Use this for initialization
+	void Start() {
+		direction = Vector2.zero;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		Move();
+	}
+	public void Move() {
+			transform.Translate(direction*speed*Time.deltaTime);
+	}
+}
--- Unity Proyect/Rebirth-2/Assets/Scripts/Player.cs
+++ Unity Proyect/Rebirth-2/Assets/Scripts/Player.cs
@@ -2,12 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingScript : MonoBehaviour {
+public class Player : Character {
 
-	[SerializeField]
-	private float speed;
 
-	private Vector2 direction;
 	// Use this for initialization
 	void Start() {
 		direction = Vector2.zero;
@@ -16,11 +13,9 @@ public class MovingScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update() {
 		GetInput();
-		MovePlayer();
+		Move();
 	}
-  public void MovePlayer() {
-			transform.Translate(direction*speed*Time.deltaTime);
-	}
+
 	public void GetInput() {
 		direction = Vector2.zero;
 		if (Input.GetKey(KeyCode.W)) {
