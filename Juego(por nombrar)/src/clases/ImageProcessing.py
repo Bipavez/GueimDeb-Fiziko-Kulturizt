a5ff65de86c006172e61888e4a8740dabd749f7a
Benjamin Pavez <bipavez@uc.cl>
2018-07-15 12:14:14 -0400

    For backup

--- Juego(por nombrar)/src/clases/ImageProcessing.py
+++ Juego(por nombrar)/src/clases/ImageProcessing.py
@@ -12,27 +12,16 @@ def rotate_image(mat, angle):
     """
     Rotates an image (angle in degrees) and expands image to avoid cropping
     """
-
     height, width, bytes = mat.shape
-    image_center = (width/2, height/2) # getRotationMatrix2D needs coordinates in reverse order (width, height) compared to shape
-
+    image_center = (width/2, height/2)
     rotation_mat = cv.getRotationMatrix2D(image_center,  angle, 1.)
-
-    # rotation calculates the cos and sin, taking absolutes of those.
     abs_cos = abs(rotation_mat[0,0])
     abs_sin = abs(rotation_mat[0,1])
-
-    # find the new width and height bounds
     bound_w = int(height * abs_sin + width * abs_cos)
     bound_h = int(height * abs_cos + width * abs_sin)
-
-    # subtract old image center (bringing image back to origo) and adding the new image center coordinates
     rotation_mat[0, 2] += bound_w/2 - image_center[0]
     rotation_mat[1, 2] += bound_h/2 - image_center[1]
-
-    # rotate image with the new bounds and translated rotation matrix
     rotated_mat = cv.warpAffine(mat, rotation_mat, (bound_w, bound_h))
-
     return rotated_mat
 class Shadow(sprite.Sprite): #clase base para las sombras, se maneitenen poco tiempo en memoria
     def __init__(self, parent, alpha,  angle):
@@ -61,18 +50,15 @@ class Shadow(sprite.Sprite): #clase base para las sombras, se maneitenen poco ti
         im_bw = cv.erode(im_bw, kernel ,iterations = 1) #se erosiona la imagen obteniendo una silueta más robusta del personaje
         surf = pg.surfarray.make_surface(im_bw) #creación de superficie pygame
         surf.set_alpha(alpha) #bueno, la transparencia
-
         return surf
     def rotation(self, angle):
         return rotate_image(self.array, np.degrees(angle)) #para orden, limpiar
-
     @property
     def image(self):
         dst  = self.rotation(self.angle) #se rota la figura
         img = self.get_shadow(dst, self.alpha).convert() #se convierte la figura para compatibilidad con las operaciones normales de pygame
         img.set_colorkey((255,255,255)) #se hace transparente el blanco
         return img
-
 def draw_fog(size, depth):
     fog = pg.Surface(size, pg.SRCALPHA)
     fog.fill((0,0,0,255))
--- Juego(por nombrar)/src/clases/main.py
+++ Juego(por nombrar)/src/clases/main.py
@@ -3,4 +3,4 @@ from settings import H, W
 
 App1 = App((W, H), "animations/background.png", [])
 while True:
-    App1.mainLoop() #mainLoop de la aplicación
+    App1.mainLoop()  # mainLoop de la aplicación
