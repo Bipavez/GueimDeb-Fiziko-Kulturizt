af33cdcf05bfa9f9fa9c58ccf69c9258cfc210ee
Benjamin Pavez <bipavez@uc.cl>
2018-07-17 02:49:24 -0400

    zdcvd

--- Unity Proyect/Rebirth-2/Assets/Scripts/Character.cs
+++ Unity Proyect/Rebirth-2/Assets/Scripts/Character.cs
@@ -6,18 +6,35 @@ public abstract class Character : MonoBehaviour {
 
 	[SerializeField]
 	private float speed;
-
+    [SerializeField]
+    protected float speedMod;
 	protected Vector2 direction;
+    private float recoverySpeed;
 	// Use this for initialization
+    float getSpeed()
+    {
+        return speed * speedMod;
+    }
 	void Start() {
+        recoverySpeed = 1.5f;
+        speedMod = 1f;
 		direction = Vector2.zero;
 	}
 
 	// Update is called once per frame
 	protected virtual void Update() {
+        if (speedMod < 1f)
+        {
+            speedMod += 3f*Time.unscaledDeltaTime;
+
+        }
+        if (speedMod > 1f)
+        {
+            speedMod = 1f;
+        }
 		Move();
 	}
 	public void Move() {
-			transform.Translate(direction*speed*Time.deltaTime);
+			transform.Translate(direction*getSpeed()*Time.deltaTime);
 	}
 }
--- Unity Proyect/Rebirth-2/Assets/Scripts/Player.cs
+++ Unity Proyect/Rebirth-2/Assets/Scripts/Player.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Player : Character {
 
+    [SerializeField]
+    private Animator anim;
 
+    private bool playerWalking;
+
+    private Vector2 lastMoved;
+
+   
 	// Use this for initialization
 	void Start() {
 		direction = Vector2.zero;
+        anim = GetComponent<Animator>();
 	}
 
 	// Update is called once per frame
@@ -20,22 +28,53 @@ public class Player : Character {
 
 	public void GetInput()
 	{
+        playerWalking = false;
 		direction = Vector2.zero;
+        
+
+
 		if (Input.GetKey(KeyCode.W))
 		{
 			direction += Vector2.up;
-		}
-		if (Input.GetKey(KeyCode.S))
+            playerWalking = true;
+            lastMoved = new Vector2(direction.x, direction.y);
+
+        }
+        if (Input.GetKey(KeyCode.S))
 		{
 			direction += Vector2.down;
-		}
+            playerWalking = true;
+            lastMoved = new Vector2(direction.x, direction.y);
+        }
 		if (Input.GetKey(KeyCode.D))
 		{
 			direction += Vector2.right;
-		}
-		if (Input.GetKey(KeyCode.A)) {
-			direction += Vector2.left;
 
-		}
-	}
+            playerWalking = true;
+            lastMoved = new Vector2(direction.x, direction.y);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+            playerWalking = true;
+            lastMoved = new Vector2(direction.x, direction.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("Attack");
+            base.speedMod = 0.0001f;
+
+        }
+        
+
+
+
+        anim.SetFloat("MoveX", direction.x);
+        anim.SetFloat("MoveY", direction.y);
+        anim.SetBool("Walking", playerWalking);
+        anim.SetFloat("MovedX", lastMoved.x);
+        anim.SetFloat("MovedY", lastMoved.y);
+    }
 }
+
